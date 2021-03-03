9195d9c0c97434fa48a60609ba7971b2350a0bb7
cardiffinc <61339666+cardiffinc@users.noreply.github.com>
2021-03-03 18:32:43 +0530

    Update snackbar.js

--- snackbar.js
+++ snackbar.js
@@ -1,9 +1,11 @@
 //param 'message' is the message that'll be displayed in the snackbar
+//also make sure a div with id snackbar is in the DOM. 
 function showSnackbar(message) {
     var snackbar = document.getElementById("snackbar");
     snackbar.innerHTML = message;
     snackbar.className = "show";
     setTimeout(function(){ 
         snackbar.className = snackbar.className.replace("show", ""); 
+        snackbar.innerHTML = '';
     }, 3000);
   }
